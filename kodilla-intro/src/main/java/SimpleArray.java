2922f7e8b95d021065df8981051952497445a7b3
SebastianM <muszynskiseb@gmail.com>
2023-01-31 23:07:40 +0100

    tablice

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,16 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        String[] bands = new String[4];
+        bands [0] = "Black Sabbath";
+                bands [1] = "Pantera";
+                        bands [2] = "Malevolence";
+                                bands [3] = "Sleep Token";
+                                        String band = bands [3];
+        System.out.println(band);
+
+        int numberOfbands = bands.length;
+        System.out.println("Moja tablica zawiera " +numberOfbands+ " elementy");
+
+    }
+}
