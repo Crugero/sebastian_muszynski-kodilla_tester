302adbb81a2c68e8c8c811e7d1e214583dfdf0f2
SebastianM <muszynskiseb@gmail.com>
2023-01-31 18:03:04 +0100

    ZmienneModul08

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,14 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myFavGuitar = "Jericho Baritone Telecaster";
+        int howManyGuitars = 3;
+        double favScaleLength = 27;
+        String playerClass = "TOP";
+        boolean isBlackSabbathBest = true;
+        System.out.println(myFavGuitar);
+        System.out.println(howManyGuitars);
+        System.out.println(favScaleLength);
+        System.out.println(playerClass);
+        System.out.println(isBlackSabbathBest);
+    }
+}
