e3ad1dd06be107dadbc9ad2efc847915a7a1dfc4
SebastianM <muszynskiseb@gmail.com>
2023-02-11 15:05:31 +0100

    Check Weight, price and stability

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,20 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook(1, 1000);
+        Notebook notebook = new Notebook(1, 1000, 2019);
         System.out.println("Weight is: " + notebook.weight + " Price is: " + notebook.price);
         notebook.checkPrice();
         notebook.checkWeight();
+        notebook.checkPriceStability();
 
-        Notebook heavyNotebook = new Notebook(9, 1500);
+        Notebook heavyNotebook = new Notebook(9, 1500, 2024);
         System.out.println("Weight is: " + heavyNotebook.weight + " Price is: " + heavyNotebook.price);
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
+        heavyNotebook.checkPriceStability();
 
-        Notebook oldNotebook = new Notebook(4, 500);
+        Notebook oldNotebook = new Notebook(4, 500, 2017);
         System.out.println("Weight is: " + oldNotebook.weight + " Price is: " + oldNotebook.price);
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
+        oldNotebook.checkPriceStability();
 
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,10 +1,12 @@
 public class Notebook {
     int weight;
     int price;
+    int year;
 
-    public Notebook(int weight, int price){
+    public Notebook(int weight, int price, int year){
         this.weight = weight;
         this.price = price;
+        this.year = year;
 
     }
 
@@ -29,6 +31,26 @@ public class Notebook {
         }
         else{
             System.out.println("This notebook is too heavy");
+        }
+    }
+    public void checkPriceStability(){
+        if (this.price < 500 && this.year > 2020 && this.year < 2024){
+            System.out.println("This notebook got cheap quickly");
+        }
+        else if (this.price >= 500 && this.price < 1000 && this.year <= 2020){
+            System.out.println("This notebook keeps it's price very well");
+        }
+        else if (this.price >= 1000 && this.year > 2020 && this.year < 2024) {
+            System.out.println("This notebook has good price for it's production year");
+        }
+        else if (this.price >= 1000 && this.year < 2020) {
+            System.out.println("This notebook can be considered as great investment!");
+        }
+        else if (this.year > 2023) {
+            System.out.println("Hey, are You from the future?");
+        }
+        else{
+            System.out.println("It's hard to tell anything about the price considering this production year");
         }
     }
 
