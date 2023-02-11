80f80011f539fd3be8e12b5fa949da4f76a3004e
SebastianM <muszynskiseb@gmail.com>
2023-02-11 14:45:08 +0100

    Check price and weight

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,17 +1,20 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook(600, 1000);
-        System.out.println(notebook.weight + " " + notebook.price);
+        Notebook notebook = new Notebook(1, 1000);
+        System.out.println("Weight is: " + notebook.weight + " Price is: " + notebook.price);
         notebook.checkPrice();
+        notebook.checkWeight();
 
-        Notebook heavyNotebook = new Notebook(2000, 1500);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook(9, 1500);
+        System.out.println("Weight is: " + heavyNotebook.weight + " Price is: " + heavyNotebook.price);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
 
-        Notebook oldNotebook = new Notebook(1600, 500);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        Notebook oldNotebook = new Notebook(4, 500);
+        System.out.println("Weight is: " + oldNotebook.weight + " Price is: " + oldNotebook.price);
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
 
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,6 +1,7 @@
 public class Notebook {
     int weight;
     int price;
+
     public Notebook(int weight, int price){
         this.weight = weight;
         this.price = price;
@@ -8,11 +9,26 @@ public class Notebook {
     }
 
     public void checkPrice(){
-        if (this.price < 900){
+        if (this.price < 600){
             System.out.println("This notebook is cheap");
         }
+        else if (this.price >= 600 && this.price <= 1000){
+            System.out.println("The price is good");
+            }
         else{
             System.out.println("This notebook is quite expensive");
+        }
+    }
+
+    public void checkWeight(){
+        if (this.weight < 2){
+            System.out.println("This notebook seems strangely light");
+        }
+        else if (this.weight >= 2 && this.weight <= 5){
+            System.out.println("This notebook has good weight");
+        }
+        else{
+            System.out.println("This notebook is too heavy");
         }
     }
 
