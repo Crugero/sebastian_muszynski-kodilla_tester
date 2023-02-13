f6dce716ea484342e0f029d23a32f0a8b43d1412
SebastianM <muszynskiseb@gmail.com>
2023-02-13 18:49:04 +0100

    Grades poprawa

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -8,14 +8,8 @@ public class Grades {
         grades = new int[10];
         size = 0;
     }
-    //public void add(int value) {           //A to nic nie zwraca, bo jest void? :/ Po co this. przy każdej zmiennej jak jest tylko 1 o tej nazwie?
-    //   if (this.size == 10) {
-    //       return;
-    //   }
-    //  this.grades[this.size] = value;
-    //   this.size++;
-    //}
-    public int add(int value) {           //Moje ADD
+
+    public int add(int value) {
         if (size == 10) {
             System.out.println("Nie można dodać więcej ocen");
             return size;
@@ -24,15 +18,17 @@ public class Grades {
         size++;
         return size;
     }
+
     public int lastAddedGrade() {
         if (size == 0) {
             System.out.println("Nie dodano żadnej oceny");
             return 0;
         }
-        int lastAdded = grades[size - 1]; //tu jest coś skopane chyba
+        int lastAdded = grades[size - 1];
         System.out.println("Ostatnio dodana ocena to " + lastAdded);
         return lastAdded;
     }
+
     private void sumGrades() {   //Suma
         int sum = 0;
         for (int i = 0; i < grades.length; i++) {
@@ -40,7 +36,8 @@ public class Grades {
         }
         this.sum = sum;
     }
-    public double averageGrades() {   //Średnia - osobna metoda wywołująca sumę? Działa
+
+    public double averageGrades() {
         sumGrades();
         double average = (double)sum / size;
         System.out.println("Średnia ocen: " + average);
