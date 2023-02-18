abf8f9da3ed1261b171c1fce77f1d3b81e7fb117
Sebastian <muszynskiseb@gmail.com>
2023-02-18 16:18:09 +0100

    Cwiczenie metody statyczne

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title){  //Tak czy siak muszę utworzyć obiekt?
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title){ //Teoretycznie mam konstruktor, więc po co metoda? Nie kleje :<
+        Book book = new Book(author, title);
+        System.out.println(book);
+        return book;
+    }
+}
---
+++ kodilla-intro/src/main/java/Bookprint.java
@@ -0,0 +1,5 @@
+public class Bookprint {
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+    }
+}
