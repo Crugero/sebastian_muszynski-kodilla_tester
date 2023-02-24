c787b5622e360caef4a850c7d356bfc7422def04
Sebastian <muszynskiseb@gmail.com>
2023-02-24 12:02:34 +0100

    BeforeEach i przesuniecie zmiennych

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.basic_assertion;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,36 +8,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTestSuite {
 
     private Calculator calculator;
+    int a = 5;
+    int b = 8;
+    int c = 0;
+    int d = -2;
 
-    @BeforeAll
+    @BeforeEach
     public void setUp(){
         calculator = new Calculator();
     }
 
     @Test
     public void testSum() {
-        int a = 5;
-        int b = 8;
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
 
     @Test
     public void testSubtract() {
-        int a = 5;
-        int b = 8;
         int subResult = calculator.subtract(a, b);
         assertEquals(-3, subResult);
     }
 
     @Test
     public void testPowerWithIntegers() { //może być w jednym czy powinno być rozbite na osobne przypadki?
-        int a = 5;
-        int b = 0;
-        int c = -2;
         int powResultA = calculator.power(a);
-        int powResultB = calculator.power(b);
-        int powResultC = calculator.power(c);
+        int powResultB = calculator.power(c);
+        int powResultC = calculator.power(d);
         assertEquals(25, powResultA, 0.01);
         assertEquals(0, powResultB, 0.01);
         assertEquals(4, powResultC, 0.01);
