bbf2aaf525abc981b8dcc31b448463328de0e944
Sebastian <muszynskiseb@gmail.com>
2023-03-06 18:54:08 +0100

    klasa niemutowalna

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -0,0 +1,20 @@
+package com.kodilla.collections.adv.immutable;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
