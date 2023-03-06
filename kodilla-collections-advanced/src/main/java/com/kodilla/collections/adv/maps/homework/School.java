9c8765428780f537ee86fbe3c286b407d57d6eae
Sebastian <muszynskiseb@gmail.com>
2023-03-06 18:45:23 +0100

    szkoly i dyrektorzy

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -1,2 +1,48 @@
-package com.kodilla.collections.adv.maps.homework;public class School {
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class School {
+
+    private String name;
+    private List<Integer> numberOfStudents = new ArrayList<>();
+
+    public School(String name, List<Integer> numberOfStudents) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int countStudents() {    // [1]
+        int sum = 0;            // [2]
+        for (int students : numberOfStudents)  // [3]
+            sum += students;
+        return sum;
+    }
+
+        @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        School school = (School) o;
+        return Objects.equals(name, school.name) && Objects.equals(numberOfStudents, school.numberOfStudents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, numberOfStudents);
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", numberOfStudents=" + numberOfStudents +
+                '}';
+    }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -1,2 +1,30 @@
-package com.kodilla.collections.adv.maps.homework;public class SchoolDirectory {
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+
+    public static void main(String[] args) {
+
+        Map<Principal, School> directory = new HashMap<>();
+
+        Principal jerzyKryszak = new Principal("Jerzy", "Kryszak");
+        Principal nergalDarski = new Principal("Nergal", "Darski");
+        Principal philAnselmo = new Principal("Philip", "Anselmo");
+
+        School siodemka = new School("Super Szkoła Czegos", Arrays.asList(25, 24, 24, 25, 25));
+        School osemka = new School("Szkoła Czegos Innego", Arrays.asList(20, 19, 15, 10, 15));
+        School dziewiatka = new School("Strasznie slaba szkola", Arrays.asList(10, 8, 6, 5, 6));
+
+        directory.put(jerzyKryszak, osemka);
+        directory.put(nergalDarski, dziewiatka);
+        directory.put(philAnselmo, siodemka);
+
+
+        for (Map.Entry<Principal, School> schoolPrincipal : directory.entrySet()) {
+            System.out.println(schoolPrincipal.getKey().getName() + " " + schoolPrincipal.getKey().getSurname() + " is the principal of the " + schoolPrincipal.getValue().getName() + " which has " + schoolPrincipal.getValue().countStudents() + " students.");
+        }
+    }
 }
